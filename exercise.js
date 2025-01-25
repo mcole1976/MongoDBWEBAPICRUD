7548dca32ae95fb9991065a59b3c802a898b2ed7
Marcus Cole <marcuscscole@gmail.com>
2025-01-25 09:02:24 -0600

    Update exercise.js
    remaking file after cutting wrong file

--- exercise.js
+++ exercise.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
 
     $('#updateForm').on('submit', function(event) {
         event.preventDefault();
-        var button = document.getElementById('entryB');
-        button.disabled = true;
+
         const formData = {
-            meal: $('#meal').val(),
-            calorie_Count: $('#calorie_Count').val(),
-            meal_Description: $('#meal_Description').val(),
-            date: new Date().toISOString() // Add current timestamp
+            exercise_Name: $('#Description').val(),
+            calorie_Count: $('#Calories').val(),
+            exercise_Time: $('#Time_taken').val(),
+            date: new Date().toISOString(), // Add current timestamp
+            exercise_ID: 4
             
         };
-
+        console.log(formData);
         $.ajax({
-            url: 'http://localhost:3000/add-data',
+            url: 'http://localhost:3000/add-ex',
             type: 'POST',
             contentType: 'application/json',
             headers: { 'Access-Control-Allow-Origin': '*' },
@@ -21,52 +21,23 @@ $(document).ready(function() {
             success: function(data) {
                 console.log(data);
                 clearForm();
-
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.error('Error:', errorThrown);
 
             }
         });
-
-        setTimeout(function() {
-            //alert('Button Clicked!');
-            button.disabled = false;
-        }, 1000);
-        console.log(formData);
     });
-
-    $.getJSON('/api/oneday', function (data) 
-    
-        { 
-            var rows = ''; 
-            console.log(data);
-            $.each(data, function (index, item) 
-            { 
-                rows += '<tr>'; rows += '<td>' 
-                + item.meal + '</td>'; rows += '<td>' + 
-                item.meal_Description + '</td>' 
-                + '<td>' + item.calorie_Count 
-                + '</td>'; rows += '<td>' 
-                + moment(item.date).format("MM/DD/YYYY");+ '</td>'; rows += '</tr>'; }); 
-            $('#data-table').html(rows); 
-        }
-    );
-
-
-
 });
 function clearForm() { 
     const form = document.getElementById('updateForm'); 
     if (form) 
     { 
         form.reset(); 
-        location.reload();
     } 
     else 
     { 
         console.error("Form not found with ID:"); 
     } 
 }
-
- 
+    
