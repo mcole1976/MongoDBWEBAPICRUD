7551a924c9254b560703fb09c936516117166998
Marcus Cole <marcuscscole@gmail.com>
2025-01-25 09:01:35 -0600

    Update exercise.js
    added delay to avoid double click

--- exercise.js
+++ exercise.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
     $('#updateForm').on('submit', function(event) {
         event.preventDefault();
-
+        var button = document.getElementById('entryB');
+        button.disabled = true;
         const formData = {
             meal: $('#meal').val(),
             calorie_Count: $('#calorie_Count').val(),
@@ -10,7 +11,7 @@ $(document).ready(function() {
             date: new Date().toISOString() // Add current timestamp
             
         };
-        console.log(formData);
+
         $.ajax({
             url: 'http://localhost:3000/add-data',
             type: 'POST',
@@ -19,7 +20,7 @@ $(document).ready(function() {
             data: JSON.stringify(formData),
             success: function(data) {
                 console.log(data);
-                clearForm()
+                clearForm();
 
             },
             error: function(jqXHR, textStatus, errorThrown) {
@@ -27,13 +28,40 @@ $(document).ready(function() {
 
             }
         });
+
+        setTimeout(function() {
+            //alert('Button Clicked!');
+            button.disabled = false;
+        }, 1000);
+        console.log(formData);
     });
+
+    $.getJSON('/api/oneday', function (data) 
+    
+        { 
+            var rows = ''; 
+            console.log(data);
+            $.each(data, function (index, item) 
+            { 
+                rows += '<tr>'; rows += '<td>' 
+                + item.meal + '</td>'; rows += '<td>' + 
+                item.meal_Description + '</td>' 
+                + '<td>' + item.calorie_Count 
+                + '</td>'; rows += '<td>' 
+                + moment(item.date).format("MM/DD/YYYY");+ '</td>'; rows += '</tr>'; }); 
+            $('#data-table').html(rows); 
+        }
+    );
+
+
+
 });
 function clearForm() { 
     const form = document.getElementById('updateForm'); 
     if (form) 
     { 
         form.reset(); 
+        location.reload();
     } 
     else 
     { 
@@ -41,4 +69,4 @@ function clearForm() {
     } 
 }
 
-    
+ 
